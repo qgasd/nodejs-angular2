564416bb546b1730d699b03be59e1082d3c412db
yangsenss <yangsenss@outlook.com>
2017-07-17 15:03:52 +0800

    登录页面数据传递已经package.json加入expressjs

--- static/app/login/login.component.ts
+++ static/app/login/login.component.ts
@@ -13,7 +13,25 @@ export class LoginComponent implements OnInit {
 
     ngOnInit() { }
 
-    onLoggedin() {
+    onLoggedin(name:string,password:string) {
+        // var express = require('express');
+        // var app = express();
+        
+        // app.get('/', function (req:any, res:any) {
+        // res.send('Hello World');
+        // })
+        
+        // var server = app.listen(8081, function () {
+        
+        // var host = server.address().address
+        // var port = server.address().port
+        
+        // console.log("应用实例，访问地址为 http://%s:%s", host, port)
+        
+        // })
+        if(name){
+            alert("名字是："+name+",密码是："+password);
+        }
        console.info();
         localStorage.setItem('isLoggedin', 'true');
         
